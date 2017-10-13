64c70df0aa85ba9e537cba1305854f1cb01504d2
Jing Jin <johnnyjinjing@gmail.com>
2017-10-13 14:46:02 -0700

    add pagination to listviews

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,99 @@ import markdown
 
 from .models import Post, Category, Tag
 
-class PostIndexListView(ListView):
+class _PostListView(ListView):
+    """ Paginated ListView
+    """
+    N_PAGE_ONESIDE = 2
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(_PostListView, self).get_context_data(**kwargs)
+
+        # get context provided in ListView
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        # get pagination data
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+
+        context.update(pagination_data)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        """
+        get custormized pagination data
+        For example: page = [1, 2, 3, ..., 9], current_page = 7, NPAGE = 2
+            then left = [5, 6], right = [8, 9], left_has_more = True,
+            right_has_more = False, first = True, last = False
+        """
+        if not is_paginated:
+            return {}
+
+        left = []
+        right = []
+        left_has_more = False # if ellipses is needed on the left
+        right_has_more = False # if ellipses is needed on the right
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = list(paginator.page_range)
+
+        if page_number == 1: # first page, left = [], left_has_more = False, first = False
+            if 1 + self.N_PAGE_ONESIDE < len(page_range):
+                right = page_range[1:1 + self.N_PAGE_ONESIDE]
+            else:
+                right = page_range[1:]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        elif page_number == total_pages: # last page, right = [], right_has_more = False, last = False
+            if page_number - self.N_PAGE_ONESIDE - 1 > 0:
+                left = page_range[page_number - self.N_PAGE_ONESIDE - 1:page_number - 1]
+            else:
+                left = page_range[:page_number - 1]
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+        else:
+            if page_number - self.N_PAGE_ONESIDE - 1 > 0:
+                left = page_range[page_number - self.N_PAGE_ONESIDE - 1:page_number - 1]
+            else:
+                left = page_range[:page_number - 1]
+
+            if page_number + self.N_PAGE_ONESIDE < len(page_range):
+                right = page_range[page_number:page_number + self.N_PAGE_ONESIDE]
+            else:
+                right = page_range[page_number:]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
+
+
+class PostIndexListView(_PostListView):
     """ View of Blog index page
     """
 
@@ -44,7 +136,7 @@ class PostDetailView(DetailView):
             published=True)
 
 
-class PostMonthArchiveView(MonthArchiveView):
+class PostMonthArchiveView(MonthArchiveView, _PostListView):
     """ View of Monthly Archive page
     """
 
@@ -65,7 +157,7 @@ class PostMonthArchiveView(MonthArchiveView):
     def get_context_data(self, **kwargs):
         context = super(MonthArchiveView, self).get_context_data(**kwargs)
         title_string = 'Posts published in: ' \
-            + PostMonthArchiveView.MONTH_DICT[str(self.kwargs.get('month'))] \
+            + self.MONTH_DICT[str(self.kwargs.get('month'))] \
             + ', ' + str(self.kwargs.get('year'))
         context.update({
             'title': title_string
@@ -73,7 +165,7 @@ class PostMonthArchiveView(MonthArchiveView):
         return context
 
 
-class PostCategoryListView(ListView):
+class PostCategoryListView(_PostListView):
     """ View of categoty page
     """
 
@@ -95,7 +187,7 @@ class PostCategoryListView(ListView):
         })
         return context
 
-class PostTagListView(ListView):
+class PostTagListView(_PostListView):
     """ View of tag page
     """
 
