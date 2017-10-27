0b93cf982e24ec89da791f7f718b6e5aa029569f
Jing Jin <johnnyjinjing@gmail.com>
2017-10-27 11:28:56 -0700

    use environment variable for secret key

--- django-blog/settings.py
+++ django-blog/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY =
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # CSRF_COOKIE_SECURE = True
