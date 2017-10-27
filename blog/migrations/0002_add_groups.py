ae4ae835a40c55426e04658fc2a111441899a0c4
Jing Jin <johnnyjinjing@gmail.com>
2017-10-26 21:39:57 -0700

    fix bugs

--- blog/migrations/0002_add_groups.py
+++ blog/migrations/0002_add_groups.py
@@ -1,21 +1,26 @@
 from django.db import models, migrations
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.management import create_permissions
 
 def add_group_permissions(apps, schema_editor):
+
+    for app_config in apps.get_app_configs():
+        app_config.models_module = True
+        create_permissions(app_config, apps=apps, verbosity=0)
+        app_config.models_module = None
+
     # reader
     group, created = Group.objects.get_or_create(name='reader')
 
     # writer
     group, created = Group.objects.get_or_create(name='writer')
     if created:
-        group.permissions.add(
-            [
-                can_add_category,
-                can_add_tag,
-                can_add_post,
-                can_change_post,
-                can_delete_post,
-            ])
+        permission_writer = ['add_category', 'add_tag', 'add_post',
+            'change_post', 'delete_post']
+
+        for codename in permission_writer:
+            permission = Permission.objects.get(codename=codename)
+            group.permissions.add(permission)
 
 class Migration(migrations.Migration):
 
--- django-blog/settings.py
+++ django-blog/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
 
 # Application definition
 
@@ -131,8 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'static_dev'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media
 MEDIA_URL = '/media/'
